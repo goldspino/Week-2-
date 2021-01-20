d42e8d653075b21f594cfd16017dee5652bf515b
Sathish Ramachandran <sathish.r26@gmail.com>
2021-01-20 02:07:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Conditionals Challenge 1
+x = 854320098452904702984
+if x == 3:
+  print("x is 3")
+elif x == 4:
+  print("x is 4")
+else:
+  print("x is some other number")
+#for loops Challenge 2
+animals = ["jaguar", "crocodile", "octopus", "poison dart frog", "dinosaur"]
+for x in animals:
+  print(x)
+#for loops Challenge 3
+for x in range (20):
+  print("I love Python!")
+#Challenge 4
+for x in range (26):
+  result = 5 * x
+  print("5 * " + str(x) + " is " + str(result))
